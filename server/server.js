f1edf3e1d7e34fe74e2a682cb851abfb40023294
Wilkins Chow <chowh7@student.douglascollege.ca>
2023-12-08 12:03:43 -0800

    2nd commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+//import bookRouter from "./routes/book.js";
+const app = express();
+const port = 5001;
+
+// Parse JSON bodies
+app.use(express.json());
+
+// Parse URL-encoded bodies
+//app.use(express.urlencoded({ extended: true }));
+
+//Connect to Mongo DB
+mongoose.connect("mongodb+srv://testUser003:csis3380@cluster0.fisfqei.mongodb.net/finalPracticeDB?retryWrites=true&w=majority")
+.then(() => console.log('MongoDB connected'))
+.catch(err => {
+    console.log(err)
+});
+
+app.use(cors())
+//app.use("/books", bookRouter)
+
+app.listen(port, ()=>{
+    console.log(`Server running on ${port}`)
+})
